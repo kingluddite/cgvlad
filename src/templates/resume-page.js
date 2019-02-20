ae58e5b68e203a877e44a84e2d122d1d5fb12189
kindluddite <howley.phil@gmail.com>
2019-02-20 13:25:36 -0800

    Add Google fonts and fix resume template and md file to work

--- src/templates/resume-page.js
+++ src/templates/resume-page.js
@@ -3,7 +3,14 @@ import PropTypes from 'prop-types';
 import { graphql } from 'gatsby';
 import Helmet from 'react-helmet';
 import Layout from '../components/Layout';
+
+// images
 // import logo from '../assets/images/cgvlad-logo.png';
+import pic03 from '../assets/images/pic03.jpg';
+import pic08 from '../assets/images/pic08.jpg';
+import pic09 from '../assets/images/pic09.jpg';
+import pic10 from '../assets/images/pic10.jpg';
+// import styled from 'styled-components';
 
 export const ResumePageTemplate = ({
   title,
@@ -15,12 +22,70 @@ export const ResumePageTemplate = ({
 }) => (
   <>
     <Helmet title={`${title} | `} />
-    <div className="id">
-      <h1>{my_name}</h1>
-      <h2>{my_skills}</h2>
-    </div>
+    <div id="main" className="alt">
+      <section id="one">
+        <div className="inner">
+          <header className="major">
+            <h1>
+              {my_name} | {title}
+            </h1>
+          </header>
 
-    <p className="statement">{statement}</p>
+          <h2 id="content" />
+          <p>{statement}</p>
+          <div className="grid-wrapper">
+            <div className="col-6">
+              <h3>{my_skills}</h3>
+              <p>
+                Nunc lacinia ante nunc ac lobortis. Interdum adipiscing gravida
+                odio porttitor sem non mi integer non faucibus ornare mi ut ante
+                amet placerat aliquet. Volutpat commodo eu sed ante lacinia.
+                Sapien a lorem in integer ornare praesent commodo adipiscing
+                arcu in massa commodo lorem accumsan at odio massa ac ac. Semper
+                adipiscing varius montes viverra nibh in adipiscing blandit
+                tempus accumsan.
+              </p>
+            </div>
+            <div className="col-6">
+              <h3>WORK EXPERIENCE</h3>
+              <p>
+                In arcu accumsan arcu adipiscing accumsan orci ac. Felis id enim
+                aliquet. Accumsan ac integer lobortis commodo ornare aliquet
+                accumsan erat tempus amet porttitor. Ante commodo blandit
+                adipiscing integer semper orci eget. Faucibus commodo adipiscing
+                mi eu nullam accumsan morbi arcu ornare odio mi adipiscing
+                nascetur lacus ac interdum morbi accumsan vis mi accumsan ac
+                praesent.
+              </p>
+            </div>
+            <div className="col-4">
+              <h3>EDUCATION</h3>
+              <p>
+                Nunc lacinia ante nunc ac lobortis. Interdum adipiscing gravida
+                odio porttitor sem non mi integer non faucibus ornare mi ut ante
+                amet placerat aliquet. Volutpat eu sed ante lacinia sapien lorem
+                accumsan varius montes viverra nibh in adipiscing blandit tempus
+                accumsan.
+              </p>
+            </div>
+            <div className="col-4">
+              <h3>HOBBIES</h3>
+              <p>
+                Nunc lacinia ante nunc ac lobortis. Interdum adipiscing gravida
+                odio porttitor sem non mi integer non faucibus ornare mi ut ante
+                amet placerat aliquet. Volutpat eu sed ante lacinia sapien lorem
+                accumsan varius montes viverra nibh in adipiscing blandit tempus
+                accumsan.
+              </p>
+            </div>
+            <div className="col-4">
+              <h3>REFERENCES</h3>
+              <p>Available on Request</p>
+            </div>
+          </div>
+        </div>
+      </section>
+    </div>
   </>
 );
 
@@ -42,9 +107,9 @@ const ResumePage = ({ data }) => {
         title={frontmatter.title}
         heading={frontmatter.heading}
         description={frontmatter.description}
-        my_name={frontmatter.description}
-        my_skills={frontmatter.description}
-        statement={frontmatter.description}
+        my_name={frontmatter.my_name}
+        my_skills={frontmatter.my_skills}
+        statement={frontmatter.statement}
       />
     </Layout>
   );
